0d64b1c8c4b50fd25900865219500b329830bb06
Username-lph <120899698@qq.com>
2016-11-30 22:18:46 +0800

    site

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require("express");
 var app = express();
 var mysql = require('mysql');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
 var con = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
@@ -27,11 +33,46 @@ app
 		});
 	})
 	.post('/user', function(req, res) {
-
+		var sql = 'insert into address set ?';
+		con.query(sql, {
+			name: ''
+		}, function(err, r) {
+			if(!err) {
+				res.json({
+					id: r.insertId
+				});
+			}
+		});
 	})
 	.put('/user', function(req, res) {
-
+		if(req.body.name) {
+			var sql = 'update address set name=? where id=?';
+			con.query(sql, [req.body.name, req.body.id], function(err, r) {
+				if(!err) {
+					res.json({
+						state: 'ok'
+					});
+				}
+			});
+		} else if(req.body.phone) {
+			var sql = 'update address set phone=? where id=?';
+			con.query(sql, [req.body.phone, req.body.id], function(err, r) {
+				if(!err) {
+					res.json({
+						state: 'ok'
+					});
+				}
+			})
+		}
 	})
 	.delete('/user', function(req, res) {
-
+		console.log(req.body.id);
+		var sql = 'delete from address where id=?';
+		con.query(sql, [req.body.id], function(err, r) {
+			if(!err) {
+				res.json({
+					state: 'ok'
+				});
+			}
+		});
 	})
